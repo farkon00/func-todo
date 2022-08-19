c6c79ac86d395950d0bd82c848e03aa689a00d77
farkon00 <sammer2016sammer@gmail.com>
2022-08-19 17:50:02 +0300

    Added loading and saving new todos

--- main.py
+++ main.py
@@ -1,11 +1,35 @@
 from enum import Enum, auto
+import os
 
+def parse_todo(line: str) -> tuple[bool, str]:
+    return (bool(int(line[0])), line[1:])
+def parse_rest(rest) -> list[tuple[bool, str]]:
+    match rest.find("\n"):
+        case -1: return [parse_todo(rest)]
+        case _:
+            return [parse_todo(rest[:rest.find("\n")])] + parse_rest(rest[rest.find("\n")+1:])
+
+def generate_todo_marker(completed: bool) -> str:
+    match completed:
+        case True: return "[x] "
+        case False: return "[ ] "
+
+def generate_todo_text(todos: list[tuple[bool, str]]) -> str:
+    match todos:
+        case []: return ""
+        case _:
+            return generate_todo_marker(todos[0][0]) + todos[0][1] + "\n" + generate_todo_text(todos[1:])
 
 def get_output(file):
-    return ""
+    return "Functional TODO app in python\n" + generate_todo_text(parse_rest(file)) + "\n" 
+
+def add_todo(content, inp):
+    return content + "\n" + "0" + inp
 
 def get_action(inp):
-    return lambda x, y: ""
+    match inp:
+        case "add":
+            return add_todo
 
 def get_file_content(file, inp):
     return ""
@@ -22,9 +46,10 @@ def main():
         except FileNotFoundError:
             print(get_output(""))
         with open("todo.txt", "w") as f:
-            command = input()
-            if command: break
+            command = input().strip()
+            if command == "quit": break
             f.write(get_action(command)(content, input()))
+            os.system("clear")
 
 if __name__ == "__main__":
     main()
